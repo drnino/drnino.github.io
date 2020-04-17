b225dcd5fadb73ffd1ee99144999abb659452fa4
drnino <63514242+drnino@users.noreply.github.com>
2020-04-17 01:44:03 -0500

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,20 +1,22 @@
 <?php
 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
   $message = $_POST['message'];
   $mailFrom = $_POST['mail'];
-  $dx = $_POST['dx'];
+  $subject = $_POST['dx'];
   $cx = $_POST['cx'];
 
-
   $mailTo = "dianacarolina28@icloud.com";
-  $headers = "From: ".$mailFrom;
+  $headers = array("From: ".$mailFrom, "Reply-To: dianacarolina28@icloud.com", "X-Mailer: PHP/" . PHP_VERSION);
+  $headers = implode("\r\n", $headers);
   $txt = "Has recibido un email de ".$name." .\n\n".$message;
 
-
-
-  mail($mailTo, $txt, $headers);
+  mail($mailTo, $subject, $message, $headers);
   header("Location: index.php?mailsend");
 }
 
