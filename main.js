fb8cdd3688caf8791b3da5a6a0ab83ee82cf0ec3
drnino <63514242+drnino@users.noreply.github.com>
2020-04-17 22:34:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -53,3 +53,13 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
